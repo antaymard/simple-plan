759c10c72619277ce24c0b805eebfb72d2ff8658
antaymard <antaymard@gmail.com>
2019-11-21 17:40:57 +0100

    v0.0.2 Server serves react build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser  = require('body-parser');
+const path = require('path');
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,10 +34,14 @@ projectApi(userFromTokenRouter);
 const jobApi = require('./api/jobApi.js');
 jobApi(userFromTokenRouter);
 
-// SERVER RUNNING TEST
-app.get('/', (req, res) => {
-  res.send('working');
-})
+// SERVER RUNNING
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, 'my-app/build')));
+
+// Other requests that triggers the index display
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/my-app/build/index.html'));
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => {
