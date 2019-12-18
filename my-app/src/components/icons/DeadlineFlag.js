c13cc2c3e533e880a749d7faa00d55b7fdf580d3
antaymard <antaymard@gmail.com>
2019-12-18 20:28:34 +0100

    v0.3.2

---
+++ my-app/src/components/icons/DeadlineFlag.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeadlineFlag = () => {
+    return (
+        <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 11V1L7 3.72727L1.5 5.54545" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default DeadlineFlag;
