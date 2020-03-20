75ca06cf24ce358a04db9861e6182cefb0b99ed2
antaymard <antaymard@gmail.com>
2020-03-20 01:34:11 +0100

    Added css when deadline is today

--- my-app/src/components/job/JobDates.js
+++ my-app/src/components/job/JobDates.js
@@ -5,7 +5,7 @@ import './jobDates.css';
 function JobDates(props) {
     if (props.date) {
         return (
-            <div className={"job-card-date-display" + (moment(props.date).isBefore(new Date()) ? " is-late" : "")}>
+            <div className={"job-card-date-display" + (moment(props.date).isBefore(new Date()) ? " is-late" : moment(props.date).isSame(new Date(), 'day') ? " is-today" : "")}>
                 <div className={"date-pill" + (props.type ? ' date-pill-' + props.type : "")} />
                 {moment(props.date).format("DD MMMM")}
             </div>
